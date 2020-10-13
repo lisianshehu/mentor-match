35498c1b5d69464a6cc1af0a52647ac1127aaadd
Lisian Shehu <rpglisi@gmail.com>
2020-10-12 23:18:14 -0400

    Adding signup page component

---
+++ src/signup-page/signup.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Sign up page</h1>);
+    }
+}
+ 
+export default SignUp;
