ce0e71b439a9f778885d2db3ca062b8b340bc63d
Lisian Shehu <rpglisi@gmail.com>
2020-10-14 22:18:52 -0400

    Added barebones signup page

--- src/signup-page/signup.jsx
+++ src/signup-page/signup.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import './signup.css'
 
 class SignUp extends Component {
     state = {  }
     render() { 
-        return ( <h1>Sign up page</h1>);
+        return ( 
+        <div className="signup-form">
+            <form>
+                <div class="form-group">
+                <label for="inputFirstName">First Name</label>
+                <input type="text" class="form-control" id="inputFirst" placeholder="First Name"></input>
+                </div>
+
+                <div class="form-group">
+                <label for="inputLastName">Last Name</label>
+                <input type="text" class="form-control" id="inputLast" placeholder="Last Name"></input>
+                </div>
+                
+                <div class="form-group">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4"></input>
+                </div>
+
+                <div class="form-group">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" id="inputPassword4"></input>
+                </div>
+
+        
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </form>
+        </div>
+        );
     }
 }
  
