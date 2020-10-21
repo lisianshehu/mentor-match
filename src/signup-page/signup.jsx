8e4078083a3705dd900168a74d64b1b4d3411fa8
lisianshehu <46857154+lisianshehu@users.noreply.github.com>
2020-10-21 00:11:48 -0400

    Modified form layout and design of sign-up page

--- src/signup-page/signup.jsx
+++ src/signup-page/signup.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 import './signup.css'
+import Navbar from '../navbar/navbar'
 
 class SignUp extends Component {
     constructor(props){
@@ -59,36 +60,58 @@ class SignUp extends Component {
 
     render() { 
         return ( 
-        <div className="signup-form">
-            <form onSubmit={this.handleSubmit.bind(this)} method="POST">
-                <div class="form-group">
-                <label for="inputUserName">User Name</label>
-                <input type="text" className="form-control" id="inputUser" placeholder="User Name" value={this.state.user_name} onChange={this.onUserNameChange.bind(this)}></input>
-                </div>
+        <div className="signup-page">
+            <Navbar></Navbar>
+            <div className="create-profile">
+                <div class="card text-center" className="signup-card">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Active</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">SignUp</h5>
+                        <div className="signup-form">
+                            <form onSubmit={this.handleSubmit.bind(this)} method="POST">
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputFirstName">First Name</label>
+                                    <input type="text" className="form-control" id="inputFirst" placeholder="First Name" value={this.state.firstName} onChange={this.onFirstNameChange.bind(this)}></input>
+                                </div>
 
-                <div class="form-group">
-                <label for="inputFirstName">First Name</label>
-                <input type="text" className="form-control" id="inputFirst" placeholder="First Name" value={this.state.firstName} onChange={this.onFirstNameChange.bind(this)}></input>
-                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputLastName">Last Name</label>
+                                    <input type="text" className="form-control" id="inputLast" placeholder="Last Name" value={this.state.lastName} onChange={this.onLastNameChange.bind(this)}></input>
+                                    </div>
+                                </div>
 
-                <div class="form-group">
-                <label for="inputLastName">Last Name</label>
-                <input type="text" className="form-control" id="inputLast" placeholder="Last Name" value={this.state.lastName} onChange={this.onLastNameChange.bind(this)}></input>
-                </div>
-                
-                {/* <div class="form-group">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4"></input>
-                </div> */}
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                    <label for="inputUserName">User Name</label>
+                                    <input type="text" className="form-control" id="inputUser" placeholder="User Name" value={this.state.user_name} onChange={this.onUserNameChange.bind(this)}></input>
+                                    </div>
 
-                <div class="form-group">
-                    <label for="inputPassword">Password</label>
-                    <input type="password" className="form-control" id="inputPassword" value={this.state.password} onChange={this.onPasswordChange.bind(this)}></input>
-                </div>
+                                    <div class="form-group form-group col-md-6">
+                                        <label for="inputPassword">Password</label>
+                                        <input type="password" className="form-control" id="inputPassword" value={this.state.password} onChange={this.onPasswordChange.bind(this)}></input>
+                                    </div>
+                                </div>
 
-        
-                <button type="submit" className="btn btn-primary">Sign-up!</button>
-            </form>
+                        
+                                <button type="submit" className="btn btn-primary">Sign-up!</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         );
     }
