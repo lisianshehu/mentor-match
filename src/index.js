5cef59b42d42208d0dc2de0a396f79b2537cd852
Lisian Shehu <rpglisi@gmail.com>
2020-11-09 16:38:09 -0500

    adding privateroute component and modifying router tag in index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
+import { createBrowserHistory } from 'history'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import history from './history'
+import store from './store'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+
+ReactDOM.render(<Provider store={store}><Router history={history}><App /></Router></Provider>, document.getElementById("root"));
 
 
 serviceWorker.unregister();
