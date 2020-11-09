0d5c2bb63c517f032d99b05e4de4dada8bf718ba
Lisian Shehu <rpglisi@gmail.com>
2020-11-09 16:37:26 -0500

    adding privaterouting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import SignUp from './signup-page/signup';
 import Login from './login-page/login-page';
 import Home from './home/home';
+import PrivateRoute from './privateroute/private-route'
+import ChatPage from './chat-page/chat-page'
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Route path='/' component={Home} exact></Route>
         <Route path='/signup' component={SignUp}></Route>
         <Route path='/login' component={Login}></Route>
+        <PrivateRoute path='/chatpage' component={ChatPage}></PrivateRoute>
       </Switch>
     </main>
   );
