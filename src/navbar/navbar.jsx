cbc3d77464ff6b565f13fae15d4fe01449d95483
Lisian Shehu <rpglisi@gmail.com>
2020-11-10 20:01:35 -0500

    Adding logic to display avatar and logout button navbar when user is logged in

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
 
-import SignUp from '../signup-page/signup'
-
+import store from '../store'
 
 class Navbar extends Component {
     state = {  };
@@ -28,15 +28,24 @@ class Navbar extends Component {
                     <Link className="nav-link" to={"/chatpage"}>Chat page <span class="sr-only">(current)</span></Link>                
                     </li>
                 </ul>
+                {store.getState().isLoggedIn ? 
                 <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item">
-                    <Link className="nav-link" to="/login">Login</Link>
+                    <Avatar src="../public/blank_avatar.jpg"></Avatar>
+                </li>
+                <li class="nav-item">
+                    <Link className="nav-link" to={"/logout"}>Logout</Link>
+                </li>
+                </ul> :
+                <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item">
+                    <Link className="nav-link" to={"/login"}>Login</Link>
                 </li>
                 <li class="nav-item">
                     <Link className="nav-link" to={"/signup"}>Sign-up</Link>
                 </li>
-
-                </ul>
+                </ul> 
+                }
             </div>
           </nav>
          );
