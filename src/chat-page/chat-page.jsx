c377437a4c25ea15e91fa239b0c4f02288e954ee
Lisi Shehu <lisishehu@Lisis-MBP.attlocal.net>
2020-11-19 18:20:28 -0500

    Refractored containers for chat page components and enchanced css

--- src/chat-page/chat-page.jsx
+++ src/chat-page/chat-page.jsx
@@ -1,19 +1,74 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client'
+
 import Navbar from '../navbar/navbar'
 import Searchbar from '../searchbar/search-bar'
 import UserList from '../userlist/userlist'
+import ChatInterface from '../chat-interface/chat-interface'
+import MessageList from '../message-list/message-list'
+
+import './chat-page.css'
+
+const ENDPOINT = 'http://127.0.0.1:5000/api/chat/'
+
+let socket;
 
 class ChatPage extends Component {
-    state = {  }
-    render() { 
-        return (
-          <div className='chat-page'>
-            <Navbar></Navbar>      
-            <Searchbar></Searchbar>   
-            <UserList></UserList> 
-          </div>
-        );
+  constructor(props)
+  {
+    super(props)
+    this.state = {  
+      messages: [],
     }
+    this.sendHandler = this.sendHandler.bind(this)
+
+    // add socketio server connection
+
+    // add listening of messages from server 
+  }
+
+  sendHandler(message)
+  {
+    console.log("in send handler")
+    const messageObject = {
+      username: localStorage.getItem('user'),
+      message,
+      fromMe: true
+    };
+
+    console.log(messageObject)
+
+    // emit with socket io
+
+    this.addMessage(messageObject);
+  }
+
+  addMessage(message)
+  {
+    console.log("In add message")
+    console.log(message)
+    const messages = this.state.messages;
+    messages.push(message);
+    console.log(messages);
+    this.setState({ messages })
+  }
+
+
+  render() { 
+      return (  
+        <div className='chat-page'>
+          <Navbar></Navbar>
+          <div className='chat-outer-container'>
+            <UserList></UserList>
+            <div className="chat-container">
+              <Searchbar></Searchbar>   
+              <MessageList messages={this.state.messages}></MessageList>
+              <ChatInterface onSend={this.sendHandler}></ChatInterface>
+            </div>
+          </div>
+        </div>
+      );
+  }
 }
  
 export default ChatPage;
