ea43b85d8697d99c069664b215a0a56e26eb39ef
Lisi Shehu <lisishehu@Lisis-MBP.attlocal.net>
2020-11-20 23:56:01 -0500

    Making uselist into class for access to props of chat-page component

--- src/userlist/userlist.jsx
+++ src/userlist/userlist.jsx
@@ -2,20 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './userlist.css'
 
-const ConnectedUserList = ({userList}) => (
-    <div className="user-list"> 
-        <ul class="list-group">
-            {userList.map(user => <a href="#" key={user} class="list-group-item list-group-item-action">{user}</a>)}
-        </ul> 
-    </div>
-);
 
+class ConnectedUserList extends Component{
+    constructor(props){
+        super(props)
+        console.log(this.props)
+        this.state = {
+            userList: this.props.userList
+        }
+    }
 
-const mapStateToProps = state =>
+    static getDerivedStateFromProps(nextProps, prevState)
+    {
+        return {
+            userList: nextProps.userList
+        }
+    }
+
+    showUserChat(){
+        this.props.onClick()
+    }
+
+    render () {
+        return (
+            <div className="user-list"> 
+                <ul class="list-group">
+                    {this.state.userList.map(user => <button onClick={this.showUserChat.bind(this)} key={user} class="list-group-item list-group-item-action">{user}</button>)}
+                </ul> 
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) =>
 {
     return { userList: state.addUserReducer.userList };
 };
  
-const UserList = connect(mapStateToProps)(ConnectedUserList);
 
-export default UserList;
+
+export default connect(mapStateToProps)(ConnectedUserList);
