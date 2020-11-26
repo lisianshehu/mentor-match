4674a764f47e0b4ef46e19f7f3a09d19a3a346fd
lisianshehu <rpglisi@gmail.com>
2020-11-25 23:32:06 -0500

    Modified chat page to function together with landing page

--- src/chat-page/chat-page.jsx
+++ src/chat-page/chat-page.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import io from 'socket.io-client'
 
 import Navbar from '../navbar/navbar'
-import Searchbar from '../searchbar/search-bar'
-import ConnectedUserList from '../userlist/userlist'
 import ChatInterface from '../chat-interface/chat-interface'
 import MessageList from '../message-list/message-list'
+import history from '../history'
+
 
 import './chat-page.css'
 
@@ -16,9 +16,10 @@ class ChatPage extends Component {
     super(props)
     this.state = {  
       messages: [],
-      showChatInterface: false,
+      showChatInterface: true,
       chat_endpoint: 'http://localhost:5000',
-      userToChatWith: ''
+      userToChatWith: '',
+      message: ''
     }
     
     this.currentUser = localStorage.getItem('user')
@@ -28,17 +29,17 @@ class ChatPage extends Component {
 
   componentDidMount()
   {
-    console.log("in mount")
     const { chat_endpoint } = this.state
-    console.log(chat_endpoint)
-    console.log(this.socket)
     socket = io(chat_endpoint, { transport : ['websocket'] })
+    console.log(history.location.state.userToChatWith)
+    this.joinChatRoom(history.location.state.userToChatWith)
     console.log(socket)
   }
 
 
   componentDidUpdate(prevProps, prevState)
   {
+    console.log("in component did update")
     if (prevState.chat_endpoint !== this.state.chat_endpoint)
     {
       socket = io(this.state.chat_endpoint)
@@ -55,12 +56,14 @@ class ChatPage extends Component {
     }
   }
 
-  sendHandler(message)
+  sendHandler(message, e)
   {
-    console.log(message)
-
+    e.preventDefault()
+    
+    this.setState({message : message})
     // emit with socket-io
     socket.emit('send_message', { 'current_user': localStorage.getItem('user'), 'target_user': this.state.userToChatWith, 'message' : message })
+    this.setState({message : ''})
   }
 
   enableChatInterface = (userToChatWith) =>
@@ -73,18 +76,19 @@ class ChatPage extends Component {
   joinChatRoom(userToChatWith)
   {
     console.log(userToChatWith)
+    this.setState({userToChatWith: userToChatWith})
     console.log(localStorage.getItem('user'))
     socket.emit('join', { 'current_user': localStorage.getItem('user'), 'target_user': userToChatWith })
   }
 
   render() { 
+      console.log(this.state.userToChatWith)
+      console.log(this.state.messages)
       return (  
         <div className='chat-page'>
           <Navbar></Navbar>
           <div className='chat-outer-container'>
-            <ConnectedUserList onClick={this.enableChatInterface}></ConnectedUserList>
             <div className="chat-container">
-              <Searchbar></Searchbar>   
               <MessageList messages={this.state.messages} currentUser={this.currentUser}></MessageList>
               <ChatInterface showChatInterface={this.state.showChatInterface} onSend={this.sendHandler}></ChatInterface>
             </div>
