cc5c4db5e20835c1d70ef122bbef977a643c518c
lisianshehu <rpglisi@gmail.com>
2020-11-28 17:07:14 -0500

    style changes

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -10,43 +10,48 @@ class Navbar extends Component {
     
     navStyles = 
     {
-        backgroundColor: "#98fb98"
+        backgroundColor: "#191970"
     };
+
+    itemStyles = 
+    {
+        color: "#FFFAFA"
+    }
 
     render() { 
         return (
             <nav class="navbar navbar-expand-lg navbar-light py-4" style={this.navStyles}>
-            <a class="navbar-brand" href="#">Mentor Match</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" style={this.itemStyles} href="#">Mentor Match</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContentn" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                    <Link className="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>                
+                    <Link className="nav-link" style={this.itemStyles} to="/">Home <span class="sr-only">(current)</span></Link>                
                     </li>
                     <li class="nav-item active">
-                    <Link className="nav-link" to={"/landing-page"}>Profile<span class="sr-only">(current)</span></Link>                
+                    <Link className="nav-link" style={this.itemStyles} to={"/landing-page"}>Profile<span class="sr-only">(current)</span></Link>                
                     </li>
                 </ul>
                 {store.getState().loginReducer.isLoggedIn ? 
                 <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item" className="username-item">
-                    <h6>{localStorage.getItem('user')}</h6>
+                    <h6 style={this.itemStyles}>{localStorage.getItem('user')}</h6>
                 </li>
                 <li class="nav-item">
                     <Avatar src="../public/blank_avatar.jpg"></Avatar>
                 </li>
                 <li class="nav-item">
-                    <Link className="nav-link" to={"/logout"}>Logout</Link>
+                    <Link className="nav-link" style={this.itemStyles} to={"/logout"}>Logout</Link>
                 </li>
                 </ul> :
                 <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item">
-                    <Link className="nav-link" to={"/login"}>Login</Link>
+                    <Link className="nav-link" style={this.itemStyles} to={"/login"}>Login</Link>
                 </li>
                 <li class="nav-item">
-                    <Link className="nav-link" to={"/signup"}>Sign-up</Link>
+                    <Link className="nav-link" style={this.itemStyles} to={"/signup"}>Sign-up</Link>
                 </li>
                 </ul> 
                 }
